09740de330e8cd3aad3035855ff7108a86719748
Hemant Verma <vhemant250@gmail.com>
2025-11-20 21:18:14 +0530

    ok

---
+++ Python_Assignment/John_n_his_Series.py
@@ -0,0 +1,9 @@
+a = int(input())
+d = int(input())
+n = int(input())
+
+nth_term = a + (n-1) * d
+sum_n = (n * (2*a + (n-1)*d)) / 2
+
+print(nth_term)
+print(sum_n)
---
+++ Python_Assignment/Measurement_Conversion.py
@@ -0,0 +1,11 @@
+inches = float(input())
+
+foot = inches / 12
+yard = inches / 36
+centimetre = inches * 2.54
+meter = inches / 39.37
+
+print(foot)
+print(yard)
+print(centimetre)
+print(meter)
